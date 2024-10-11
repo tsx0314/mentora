9f0f8336438a73994a98b207f2592e3d31c827eb
Ohnojiso <notxishou@gmail.com>
2024-10-12 01:57:25 +0800

    Update main page font

--- src/App.js
+++ src/App.js
@@ -14,22 +14,22 @@ function App() {
       <Box component="img" src={logo} alt="PSA Logo" sx={{ width: '200px', marginBottom: '20px' }} />
 
       {/* Welcome message */}
-      <Typography variant="h2" component="h1" gutterBottom sx={{ color: 'white' }}>
+      <Typography variant="h2" component="h1" gutterBottom sx={{ fontFamily: 'Roboto',  color: 'white' }}>
         Welcome to Mentora!
       </Typography>
 
       {/* Navigation Links */}
       <Box sx={{ display: 'flex', justifyContent: 'center', gap: '20px', marginTop: '20px' }}>
-        <Button variant="contained" color="primary" component={Link} to="/signup" sx={{ fontSize: '1.2rem', borderRadius: '50px', padding: '10px 30px' }}>
+        <Button variant="contained" color="primary" component={Link} to="/signup" sx={{ fontFamily: 'Roboto', fontSize: '1.2rem', borderRadius: '50px', padding: '10px 30px' }}>
           Signup Page
         </Button>
-        <Button variant="contained" color="primary" component={Link} to="/login" sx={{ fontSize: '1.2rem', borderRadius: '50px', padding: '10px 30px' }}>
+        <Button variant="contained" color="primary" component={Link} to="/login" sx={{ fontFamily: 'Roboto', fontSize: '1.2rem', borderRadius: '50px', padding: '10px 30px' }}>
           Login Page
         </Button>
-        <Button variant="contained" color="primary" component={Link} to="/matching" sx={{ fontSize: '1.2rem', borderRadius: '50px', padding: '10px 30px' }}>
+        <Button variant="contained" color="primary" component={Link} to="/matching" sx={{ fontFamily: 'Roboto', fontSize: '1.2rem', borderRadius: '50px', padding: '10px 30px' }}>
           Matching Page
         </Button>
-        <Button variant="contained" color="primary" component={Link} to="/resources" sx={{ fontSize: '1.2rem', borderRadius: '50px', padding: '10px 30px' }}>
+        <Button variant="contained" color="primary" component={Link} to="/resources" sx={{ fontFamily: 'Roboto', fontSize: '1.2rem', borderRadius: '50px', padding: '10px 30px' }}>
           Resource Page
         </Button>
       </Box>
