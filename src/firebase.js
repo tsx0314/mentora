aae5f47bd30b198f610b914488479a22fbcc44b4
Ohnojiso <notxishou@gmail.com>
2024-10-12 23:13:30 +0800

    Update firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics"; // Added import for getAnalytics
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,7 +21,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const analytics = getAnalytics(app); // Initialize Analytics
 const auth = getAuth(app);
 const db = getFirestore(app);
 
